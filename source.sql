899d2c8d522018da6d40e11d8ad44e8a89f50f9f
EmilieLamps <emilie.lamps@gmail.com>
2020-01-21 20:28:34 +0100

    fin de l'exercice

---
+++ source.sql
@@ -0,0 +1,6 @@
+-- Dans la table languages, supprimer toutes les lignes parlant de HTML.
+DELETE FROM `webDevelopment` WHERE `languages`='HTML';
+-- Dans la table frameworks, modifier toutes les lignes ayant le framework Symfony par Symfony2.
+UPDATE `frameworks` SET framework='Symfony2' WHERE framework='Symfony';
+-- Dans la table languages, modifier la ligne du langage JavaScript version 5 par la version 5.1
+UPDATE `languages` SET `version`='version 5.1' WHERE id=1;
